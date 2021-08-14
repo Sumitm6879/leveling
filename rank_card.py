0ad0d7c8c2b047d8c41d74b5c91ffc68f9485534
Sumitm6879 <76704034+Sumitm6879@users.noreply.github.com>
2021-08-14 23:08:11 +0530

    Update rank_card.py

--- rank_card.py
+++ rank_card.py
@@ -228,9 +228,9 @@ class rank_card(commands.Cog):
             embed = discord.Embed(title='Backgrounds', description="There are only 2 backgrounds:",
                                   color=0xf47fff)
             if user is None:
-                embed.description += "\n✔ 1 ~-~> **Default**\n〰 2 ~-~> **Black**"
+                embed.description += "\n🟢 1 ~-~> **Default**\n⚫ 2 ~-~> **Black**"
             else:
-                embed.description += "\n〰 1 ~-~> **Default**\n✔ 2 ~-~> **Black**"
+                embed.description += "\n⚫ 1 ~-~> **Default**\n🟢 2 ~-~> **Black**"
             embed.set_footer(text=f'{self.bot.command_prefix}bg [name]', icon_url=self.bot.user.avatar_url)
             return await ctx.send(embed=embed)
         if bg.lower() in ['black', 'dark', 'blk']:
