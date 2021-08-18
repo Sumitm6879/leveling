c504d6a4ba40b8068eb4837c76a2fba756346f1a
Sumitm6879 <76704034+Sumitm6879@users.noreply.github.com>
2021-08-18 17:45:44 +0530

    Update leveling.py

--- leveling.py
+++ leveling.py
@@ -69,7 +69,7 @@ async def ping(ctx):
     embed.set_footer(text=f"{bot.command_prefix}help to get more info.", icon_url=bot.user.avatar_url)
     await ctx.send(embed=embed)
 
-@slash.slash(name='level', description='Shows the level', aliases='lvl')
+@slash.slash(name='level', description='Check your level or the level of a member')
 async def level(ctx, member: discord.Member = None):
     if member is None:
         member = ctx.author
