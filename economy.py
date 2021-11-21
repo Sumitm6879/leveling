156e6683f63370909ef3bee25b6d4f17b3e00d72
Sumitm6879 <76704034+Sumitm6879@users.noreply.github.com>
2021-11-22 00:15:46 +0530

    done

--- economy.py
+++ economy.py
@@ -1,10 +1,45 @@
 import discord
 from discord.ext import commands
+from pymongo import MonoClient
+
+P = 'sumitm6879sm'
+cluster = MongoClient(
+    f"mongodb+srv://{P}:sm6879sm@sambot.ipbu6.mongodb.net/SamBot?retryWrites=true&w=majority"
+)
+leveling = cluster['MysticBot']['levels']
+profile = cluster['Economy']['economy-profile']
 
 class Economy(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Economy Ready!")
+    
+    @commands.command()
+    async def start(self, ctx):
+        stats = profile.find_one({"_id":ctx.author.id})
+        if stats is None:
+            new_player = {"_id":ctx.author.id, "wallet":100, "bank":0}
+            profile.insert_one(new_player)
+            await ctx.send(f"You are all set **{ctx.author.name}**")
+        else:
+            return
+    
+    @commands.command()
+    async def balance(self, ctx):
+        search = player_search(ctx.author.id)
+        if search:
+            pass
 
 
 def setup(bot):
     bot.add_cog(Economy(bot))
+
+def player_search(player_id):
+    player = profile.find_one({"_id": player_id})
+    if player:
+        return True
+    else:
+        return False
