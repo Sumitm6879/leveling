42ee318489d0e336ed3b1279b1b00a13d9327528
Sumitm6879 <76704034+Sumitm6879@users.noreply.github.com>
2021-11-25 16:10:11 +0530

    making bot make some sense

--- Game/economy.py
+++ Game/economy.py
@@ -10,6 +10,14 @@ cluster = MongoClient(
 )
 leveling = cluster['MysticBot']['levels']
 profile = cluster['Economy']['economy-profile']
+imoc = cluster['Economy']['economy-in_middle_of_command']
+global_multi = cluster['Economy']['economy=GLOBAL']
+#Globals specification
+gM = global_multi.find_one({"_id": "globalMulti"})
+globalMultilplier = gM['globalmultiplier']
+
+#currency 
+coin_emoji = '🪙'
 
 #embed specifications
 embed_color = 0x2a72f7
@@ -19,7 +27,21 @@ failed_beg_choice = [
     "Lmao you didn't find anyone",
     "you broke? badluck!",
     "No Begging in the server you wanna get mute?",
-    "You looked around but there are no people around"]
+    "you looked around but there are no people to beg"]
+
+#roam choices 
+roam_choices = [
+    "while roaming you found a wallet droped on the street",
+    "you hleped police in catching a thief",
+    "you stole money from a kid! very bad",
+    "a rich man walks up to you and was very pleased to talk to you",
+    "you helped a thief escape!",
+    "you stole your friends money!",
+    "a drunk man was alseep on the bench **||you robed him||**",
+    "you got into a street fight, You won!",
+    "a blind man has lost his stick you helped him\nit was a tik tok video",
+    "you feel too lazy to walk so you returned home but you found your lost wallet in you pants ||it was never lost||"
+]
 
 # gamble emojis
 emoji_list = ["🔥", "⚡", "✨", "💎", "🍀", "💯", "🎁", "🪙"]
@@ -37,7 +59,8 @@ class Economy(commands.Cog):
     @commands.command()
     async def start(self, ctx):
         stats = profile.find_one({"_id":ctx.author.id})
-        if stats is None:
+        index = leveling.find_one({"_id": ctx.author.id})
+        if stats is None and index:
             new_player = {"_id":ctx.author.id, "wallet":100, "bank":0}
             profile.insert_one(new_player)
             await ctx.send(f"You are all set **{ctx.author.name}**")
@@ -47,12 +70,21 @@ class Economy(commands.Cog):
 
     @commands.command(aliases=['bal', 'profile'])
     async def balance(self, ctx, member: discord.Member=None):
+        if member.bot:
+            return await ctx.send(f"This does not works for bots {ctx.author.mention}")
         if member is None and ctx.message.reference:
             msg = await ctx.channel.fetch_message(id=ctx.message.reference.message_id)
             member = msg.author
         if member is None:
             member = ctx.author
         search = player_search(member.id)
+        imoc_check = find_imoc(member.id)
+        if imoc_check is False:
+            if member == ctx.author:
+                return await ctx.send(f"{member.mention} you can't do this end your previous command!")
+            else:
+                return await ctx.send(f"{ctx.author.mention} you can't do this {member.name} is in a middle of command!")
+
         if search:
             stats = profile.find_one({"_id": member.id})
             wallet, bank = stats['wallet'], stats['bank']
@@ -71,6 +103,9 @@ class Economy(commands.Cog):
     async def deposit(self, ctx, money:str):
         search = player_search(ctx.author.id)
         stats = profile.find_one({"_id": ctx.author.id})
+        imoc_check = find_imoc(ctx.author.id)
+        if imoc_check is False:
+            return await ctx.send(f"{ctx.author.mention} you can't do this end your previous command!")
         if search:
             if money == "all":
                 money = stats['wallet']
@@ -83,7 +118,7 @@ class Economy(commands.Cog):
                     new_bank = stats['bank'] + money
                     profile.update_one({"_id": ctx.author.id}, {"$set":{"wallet": new_wallet}})
                     profile.update_one({"_id": ctx.author.id}, {"$set":{"bank": new_bank}})
-                    await ctx.send(f"**{ctx.author.name}** deposited 🪙 **{money}**")
+                    await ctx.send(f"**{ctx.author.name}** deposited {coin_emoji} **{money}**")
                 else:
                     return await ctx.send("Check your wallet **{ctx.author.name}** Lmao!")
             else:
@@ -96,6 +131,10 @@ class Economy(commands.Cog):
     async def withdraw(self, ctx, money:str):
         search = player_search(ctx.author.id)
         stats = profile.find_one({"_id": ctx.author.id})
+        imoc_check = find_imoc(ctx.author.id)
+        if imoc_check is False:
+            return await ctx.send(f"{ctx.author.mention} you can't do this end your previous command!")
+
         if search:
             if money.lower() == "all":
                 money = stats['bank']
@@ -108,7 +147,7 @@ class Economy(commands.Cog):
                     new_bank = stats['bank'] - money
                     profile.update_one({"_id": ctx.author.id}, {"$set":{"wallet": new_wallet}})
                     profile.update_one({"_id": ctx.author.id}, {"$set":{"bank": new_bank}})
-                    await ctx.send(f"**{ctx.author.name}** 🪙 **{money}** withdrawn")
+                    await ctx.send(f"**{ctx.author.name}** {coin_emoji} **{money}** withdrawn")
                 else:
                     return await ctx.send("**{ctx.author.name}** duh! check your bank and try again")
             else:
@@ -118,12 +157,16 @@ class Economy(commands.Cog):
         
 
     @commands.command()
-    @commands.cooldown(1, 180, commands.BucketType.user)
+    @commands.cooldown(1, 120, commands.BucketType.user)
     async def beg(self, ctx):
         search = player_search(ctx.author.id)
+        imoc_check = find_imoc(ctx.author.id)
+        if imoc_check is False:
+            return await ctx.send(f"{ctx.author.mention} you can't do this end your previous command!")
+
         if search:
             chance1 = random.randint(1,11)
-            if chance1 in range(2,9):
+            if chance1 in range(2,10):
                 index = leveling.find_one({"_id": ctx.author.id})
                 stats = profile.find_one({"_id": ctx.author.id})
                 if index is None:
@@ -131,10 +174,10 @@ class Economy(commands.Cog):
                     return await ctx.send(f"{failed_sentence}")
                 xp = index['xp']
                 level = calculate_level(xp)
-                beg_money = level*(random.randint(1,3))+random.randint((level//10),level)
+                beg_money = (level*(random.randint(1,3))+random.randint((level//10),level))*globalMultiplier
                 new_wallet = stats['wallet'] + beg_money
                 profile.update_one({"_id": ctx.author.id}, {"$set":{"wallet":new_wallet}})
-                await ctx.send(f"**{ctx.author.name}** you earned 🪙 **{beg_money}**")
+                await ctx.send(f"**{ctx.author.name}** you earned {coin_emoji} **{beg_money}**")
             else:
                 failed_sentence = random.choice(failed_beg_choice)
                 await ctx.send(f"**{ctx.author.name}** {failed_sentence}")
@@ -152,16 +195,51 @@ class Economy(commands.Cog):
             await ctx.send(embed=em)
     
 
+    @commands.command()
+    @commands.cooldown(1, 300, commands.BucketType.user)
+    async def roam(self, ctx):
+        search = player_search(ctx.author.id)
+        imoc_check = find_imoc(ctx.author.id)
+        if imoc_check is False:
+            return await ctx.send(f"{ctx.author.mention} you can't do this end your previous command!")
+        
+        if search:
+            index = profile.find_one({"_id": ctx.author.id})
+            xp = index['xp']
+            level = calculate_level(xp)
+            roam_money = (level*random.randint(10,15)+random.randint((level//10), level))*globalMultiplier
+            roamActionChoice = random.choice(roam_choices)
+            await ctx.send(f"**{ctx.author.name}** {roamActionChoice}\n**{ctx.author.name}** you get {coin_emoji} {roam_money}")
+        else:
+            command = self.bot.get_command('roam')
+            command.reset_cooldown(ctx)
+            welcm = new_to_this(ctx)
+            await ctx.send(welcm)
+
+
+
     @commands.command(aliases=['cd'])
     async def cooldown(self, ctx):
-        beg = self.bot.get_command('beg') # get command
-        beg_cd = beg_cooldown(ctx, beg) # get command CD
-        
-        embed = discord.Embed(
-            description = f"{beg_cd}",
-            color = embed_color)
-        embed.set_author(name=f"{ctx.author.name}'s cooldown", icon_url=ctx.author.avatar_url)
-        await ctx.send(embed=embed)
+        search = player_search(ctx.author.id)
+        imoc_check = find_imoc(ctx.author.id)
+        if imoc_check is False:
+            return await ctx.send(f"{ctx.author.mention} you can't do this end your previous command!")
+        if search:
+            earning_commands_name = ['beg', 'roam']
+            earnings_cd = ""
+            for x in earning_commands_name:
+                command = self.bot.get_command(f'{x}')
+                command_cd = get_earning_cd(ctx, command, x)
+                earnings_cd += f"{command_cd}\n"
+            
+            embed = discord.Embed( 
+                description = f"{coin_emoji} **Earnings**\n{earnings_cd}",
+                color = embed_color)
+            embed.set_author(name=f"{ctx.author.name}'s cooldown", icon_url=ctx.author.avatar_url)
+            await ctx.send(embed=embed)
+        else:
+            tada = new_to_this(ctx)
+            return await ctx.send(tada)
 
     
     @commands.command(aliases=['slot'])
@@ -169,24 +247,29 @@ class Economy(commands.Cog):
     async def slots(self, ctx, money:str):
         search = player_search(ctx.author.id)
         stats = profile.find_one({"_id": ctx.author.id})
+        imoc_check = find_imoc(ctx.author.id)
+        if imoc_check is False:
+            return await ctx.send(f"{ctx.author.mention} you can't do this end your previous command!")
         if search:
             if money.lower() == 'all':
                 money = stats['wallet']
             else:
                 money = convert_str_to_number(money)
+            
+            emojis = get_emoji()
+            slots_win = slots_calulate(emojis)
             chance1 = random.randint(0,3)
             chance2 = random.randint(0,2)
             index = leveling.find_one({"_id": ctx.author.id})
-            if index is None:
-                return await ctx.send("You don't have any level lmao you can't run this command!")
             xp = index['xp']
             level = calculate_level(xp)
             old_wallet = stats['wallet']
             if old_wallet == 0:
                 statements = ['you can only slots 1 or more coins', "you don't have any money to slots", "check you wallet lmao"]
                 return await ctx.send(f"**{ctx.author.name}** " + "{0}".format(random.choice(statements)))
+
             if money <= old_wallet:
-                if chance1 == chance2: # win situation
+                if slots_win: # win situation
                     chance3 = random.randint(0,8)
                     if chance3 == (chance2 + chance1):
                         extra_money = level*random.randint(5,10)+money
@@ -195,14 +278,13 @@ class Economy(commands.Cog):
                     win_money = money+extra_money
                     new_wallet = old_wallet + win_money
                     profile.update_one({"_id": ctx.author.id}, {"$set":{"wallet":new_wallet}})
-                    emojis = get_emoji()
-                    embed = discord.Embed(description=f"You won 🪙 **{win_money}**\n\n◖{emojis}◗", color=embed_color)
+                    embed = discord.Embed(description=f"You won {coin_emoji} **{win_money}**\n\n◖{emojis}◗", color=embed_color)
                     embed.set_author(name=f"{ctx.author.name}'s slots", icon_url=ctx.author.avatar_url)
                 else:
                     new_wallet = old_wallet - money
                     profile.update_one({"_id": ctx.author.id}, {"$set":{"wallet":new_wallet}})
                     emojis = get_emoji()
-                    embed = discord.Embed(description=f"You lost 🪙 **{money}**\n\n◖{emojis}◗", color=embed_color)
+                    embed = discord.Embed(description=f"You lost {coin_emoji} **{money}**\n\n◖{emojis}◗", color=embed_color)
                     embed.set_author(name=f"{ctx.author.name}'s slots", icon_url=ctx.author.avatar_url)
                 
                 await ctx.send(embed=embed)
@@ -223,6 +305,20 @@ class Economy(commands.Cog):
 
 def setup(bot):
     bot.add_cog(Economy(bot))
+
+
+def find_imoc(player_id):
+    index = imoc.find_one({"_id": player_id})
+    if index is None:
+        return True
+    else:
+        return False
+
+def insert_imoc(player_id):
+    imoc.insert_one({"_id": player_id})
+
+def delete_imoc(player_id):
+    imoc.delete_one({"_id": player_id})
 
 
 def player_search(player_id):
@@ -279,6 +375,13 @@ def beg_cooldown(ctx, beg):
     return time
 
 
+def get_earning_cd(ctx, command, x):
+    if command.is_on_cooldown(ctx):
+        minutes, seconds= divmod(int(beg.get_cooldown_retry_after(ctx)), 60) 
+        time = f"🕐 ~-~ `{x.capitalize()}` (**{minutes}m {seconds}s**)"
+        return time
+
+
 def get_emoji():
     emojis_l = []
     while True:
@@ -288,3 +391,17 @@ def get_emoji():
             break
     emojis = "".join(emojis_l)
     return emojis
+
+def slots_calulate(emojis):
+    win = 0
+    lose = 0
+    for emoji in emojis:
+        if emoji in ["🔥","🍀","✨", "💎",]:
+            win += 1
+        elif emoji in ["☘️","⚡","💯","🎁","🪙"]:
+            lose += 1
+    
+    if win > lose:
+        return True
+    else:
+        return False
