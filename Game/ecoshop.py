511fb66db02062797378f29e05b9389f770ff534
Sumitm6879 <76704034+Sumitm6879@users.noreply.github.com>
2021-11-30 21:43:42 +0530

    1615

--- Game/ecoshop.py
+++ Game/ecoshop.py
@@ -44,7 +44,7 @@ class EcoShop(commands.Cog):
     async def lottery_system(self):
         lot_list = lottery_list.find_one({})
         time_now = datetime.datetime.utcnow()
-        if time_now.strftime('%H:%M') == '15:12':
+        if time_now.strftime('%H:%M') == '16:15':
             await asyncio.create_task(self.lotterSystem(lot_list))
     
     async def lotterSystem(self):
