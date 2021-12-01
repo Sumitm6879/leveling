4abe71ab8dcdc7bfa0fe1bb7e62f6c05349e7785
Sumitm6879 <76704034+Sumitm6879@users.noreply.github.com>
2021-12-01 18:12:56 +0530

    save

--- Game/ecoshop.py
+++ Game/ecoshop.py
@@ -37,6 +37,7 @@ class EcoShop(commands.Cog):
     
     @commands.Cog.listener()
     async def on_ready(self):
+        pass
         #self.lottery_system.start()
     
     # @tasks.loop(seconds=60*5)
